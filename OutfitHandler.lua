8d95b89cd71f5d7f707dc1f7ffd2935b19cac813
mr.sherbs <102490963+mrsherbs@users.noreply.github.com>
2023-10-09 18:14:36 -0400

    Create OutfitHandler.lua

---
+++ OutfitHandler.lua
@@ -0,0 +1,34 @@
+local repStorage = game:GetService("ReplicatedStorage")
+local players = game:GetService("Players")
+local boards = workspace.Main.Boards
+
+local current = nil
+
+local function displayOnQueue(item)
+	local id = players:GetUserIdFromNameAsync(item)
+	
+	if id then
+		local template = boards.Queue.Template:Clone()
+		template.ImageLabel.Image = players:GetUserThumbnailAsync(id, Enum.ThumbnailType.HeadShot, Enum.ThumbnailSize.Size420x420)
+		template.Name = item
+		template.Number.Text = "#" .. tostring(#boards.Queue.SurfaceGui.Frame.Frame.ScrollingFrame:GetChildren() + 1)
+		template.ID.Value = id
+		template.Parent = boards.Queue.SurfaceGui.Frame.Frame.ScrollingFrame
+	end
+end
+
+repStorage.OutfitSystem.TryOn.OnServerEvent:Connect(function(player, humanoidDesc)
+	player.Character.Humanoid:ApplyDescription(humanoidDesc)
+end)
+
+repStorage.OutfitSystem.ResetAvatar.OnServerEvent:Connect(function(player)
+	player.Character.Humanoid:ApplyDescription(players:GetHumanoidDescriptionFromUserId(player.UserId))
+end)
+
+repStorage.OutfitSystem.Request.OnServerEvent:Connect(function(player, name)
+	displayOnQueue(name)
+end)
+
+repStorage.OutfitSystem.Teleport.OnServerEvent:Connect(function(player)
+	player.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-101.5, 5.5, -24.5))
+end)
